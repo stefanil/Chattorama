de63daf9445b89af5162964a58ded871c5d8ba47
Manuel Mauky <manuel.mauky@googlemail.com>
2014-01-17 23:52:30 +0100

    mvvmfx added

---
+++ src/main/java/de/saxsys/jfx/chattorama/ChatView.java
@@ -0,0 +1,13 @@
+package de.saxsys.jfx.chattorama;
+
+import de.saxsys.jfx.mvvm.base.view.View;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatView extends View<ChatViewModel> {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
